4ba1e4c3d76cf319b8722aaa451e051776edf157
st-182 <31894656+st-182@users.noreply.github.com>
2021-06-08 09:16:25 +0300

    yesterday's additional task and async prog. continuation

---
+++ week 9/08.06.21/Async-programavimas-primises-callbacks/script.js
@@ -0,0 +1,18 @@
+// Variables
+const usersOutput = document.querySelector("#output");
+// Promises (ES6)
+// --** simple Promise example **--
+// --** more complex Promise example **--
+// PROBLEM: need to update users lists asynchronously
+const users = [
+  { name: "Peter", surname: "Griffin" },
+  { name: "Lois", surname: "Griffin" },
+  { name: "Chris", surname: "Griffin" },
+  { name: "Meg", surname: "Griffin" },
+  { name: "Stewie", surname: "Griffin" },
+];
+// --- 1. problem solving using using callbacks (old way)
+// --- 2. problem solving using using Promises (new way)
+// Async / Await
+// --- 2.1 problem solving using using Async/await (new way)
+// ---- Async / Await / Fetch
