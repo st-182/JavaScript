1b3bb120e5b5c86bf5f4eed1c2832b60776cba82
st-182 <31894656+st-182@users.noreply.github.com>
2021-06-08 10:02:59 +0300

    some callback functions

--- week 9/08.06.21/Async-programavimas-primises-callbacks/script.js
+++ week 9/08.06.21/Async-programavimas-primises-callbacks/script.js
@@ -3,6 +3,22 @@ const usersOutput = document.querySelector("#output");
 // Promises (ES6)
 // --** simple Promise example **--
 // --** more complex Promise example **--
+let simplePromice = new Promise((resolve, reject) => {
+  const userName = `Peter`;
+  if (userName === `Peter`) {
+    resolve({ message: `Success`, name: userName });
+  } else {
+    reject(`Failed`);
+  }
+});
+console.log(`simplePromice:`);
+simplePromice
+  .then((result) =>
+    console.log(` Message: ${result.message}, name: ${result.name}`)
+  )
+  .catch((error) => console.log(error));
+console.log(`simplePromice: 2222222`);
+console.log(simplePromice);
 // PROBLEM: need to update users lists asynchronously
 const users = [
   { name: "Peter", surname: "Griffin" },
@@ -11,6 +27,32 @@ const users = [
   { name: "Meg", surname: "Griffin" },
   { name: "Stewie", surname: "Griffin" },
 ];
+
+function getUsers() {
+  setTimeout(() => {
+    let output = ``;
+
+    users.forEach((user) => {
+      output += `
+            <ul>
+            <li>Name: ${user.name}</li>
+            <li>Name: ${user.surname}</li>
+            </ul>
+            `;
+    });
+    usersOutput.innerHTML = output;
+  }, 3000);
+}
+getUsers();
+
+function crateUser(user, callback) {
+  setTimeout(() => {
+    users.push(user);
+    callback();
+  }, 3000);
+}
+crateUser({ name: `Olololo`, surname: `Sasauskas` }, getUsers);
+
 // --- 1. problem solving using using callbacks (old way)
 // --- 2. problem solving using using Promises (new way)
 // Async / Await
