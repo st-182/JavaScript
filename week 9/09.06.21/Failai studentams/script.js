720362d3de808313ff004296135033d1d86a4e25
st-182 <31894656+st-182@users.noreply.github.com>
2021-06-09 09:17:20 +0300

    start files for new lecture are ready

---
+++ week 9/09.06.21/Failai studentams/script.js
@@ -0,0 +1,71 @@
+// EXTERNAL API
+const JSONPLACEHOLDER_URI = 'https://jsonplaceholder.typicode.com/posts';
+
+// *** Variables ***
+//-- buttons
+const btnGetTextFile = document.getElementById('btn1');
+const btnGetUser = document.getElementById('btn2');
+const btnGetUsers = document.getElementById('btn3');
+const btnGetPosts = document.getElementById('btn4');
+const btnSendPost = document.getElementById('btn5');
+//-- output
+const textOutput = document.querySelector('#text');
+const userOutput = document.querySelector('#user');
+const usersOutput = document.querySelector('#users');
+const postsOutput = document.querySelector('#posts');
+
+// *** Functions ***
+//OLD Version AJAX (XMLHttpRequest())
+//-- Load Text File Information
+function loadTextFileXHR() {
+  return;
+}
+
+//-- Load User Information
+function loadUserXHR() {
+  return;
+}
+
+//-- Load Users information
+function loadUsersXHR() {
+  return;
+}
+
+//-- Load Users information
+function loadPostsXHR() {
+  return;
+}
+
+//NEW VERSION AJAX (fetch())
+// -- Getting data
+function loadPostsFETCH() {
+  return;
+}
+
+// -- Sending data
+function sendPostFETCH() {
+  return;
+}
+// *** Events ***
+btnGetTextFile.addEventListener('click', loadTextFileXHR);
+btnGetUser.addEventListener('click', loadUserXHR);
+btnGetUsers.addEventListener('click', loadUsersXHR);
+btnGetPosts.addEventListener('click', loadPostsXHR);
+btnGetPosts.addEventListener('click', loadPostsFETCH);
+btnSendPost.addEventListener('click', sendPostFETCH);
+
+/*
+    readyState Values:
+    0: request not initialized
+    1: server connection established
+    2: request received
+    3: processing request
+    4: request finished and response is ready
+    More: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+
+    HTTP Statuses
+    200: "OK"
+    403: "Forbidden"
+    404: "Not Found"
+    More: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+*/
