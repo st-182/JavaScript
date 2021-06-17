65e16c453d4ae4a10232213944e77e046bb15687
st-182 <31894656+st-182@users.noreply.github.com>
2021-06-17 14:02:26 +0300

    done with Ernestas' tasks for 17.06.2021

--- week 10/17.06.2021/usersTask/script.js
+++ week 10/17.06.2021/usersTask/script.js
@@ -1,17 +1,43 @@
-//Convert prices in https://reklama.setupad.com to dollars using the current exchange rate from https://exchangeratesapi.io/ API. The script has to work just pasting it to the Dev Tools console. Please upload the script to https://gist.github.com/
-
-// price (arrayWithPrices)
-
-const endpoint = `http://api.exchangeratesapi.io/v1/latest?access_key=d416f73c33db075c966ebe330dfb7cfc&format=1`;
-const pricesEUR = [200, 500, 1000];
-// insert endpoint and prices array
-function price(prices, endpoint) {
-  fetch(endpoint)
+// Variables
+const endpoint = `https://api.github.com/users`;
+const outputElement = document.querySelector(`#output`);
+let output = ``;
+// Functions
+async function createUsers(endpoint) {
+  await fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
-      let USDexchRate = data.rates.USD;
-      const pricesUSD = prices.map((item) => (item * USDexchRate).toFixed(2));
-      console.log(pricesUSD);
+      data.slice(0, 16).forEach((item) => {
+        output += `
+      <div class="item">
+        <img src="${item.avatar_url}">
+        <span>Tarybos narys</span>
+        <p>Labas! As esu <span class="color">${item.login
+          .charAt(0)
+          .toUpperCase()}</span>!</p>
+        <h6>Tėvynės sąjunga – Lietuvos krikščionys demokratai</h6>
+      </div>
+      `;
+      });
+      outputElement.innerHTML = output;
     });
+  addFUN();
 }
-price(pricesEUR, endpoint);
+
+function addFUN() {
+  const allItems = document.querySelectorAll(`.item`);
+  console.log(allItems);
+  allItems.forEach((item) => {
+    item.addEventListener(`click`, (e) => {
+      console.log(e.target.className);
+      let was = e.target.className;
+      e.target.className = `${e.target.className} fun`;
+      setTimeout(() => {
+        console.log(was);
+        e.target.className = `${was.trim() == `fun` ? `` : was}`;
+      }, 2000);
+    });
+  });
+}
+
+createUsers(endpoint);
