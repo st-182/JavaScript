fc5ddb287c5ece2d1979d9f2387577605e92f844
st-182 <31894656+st-182@users.noreply.github.com>
2021-06-29 09:13:42 +0300

    some more changes to calculator

--- week 11/25.06.2021/Užduotis Nr. 3/script.js
+++ week 11/25.06.2021/Užduotis Nr. 3/script.js
@@ -1,42 +1,32 @@
+// var
 const itemElements = document.querySelectorAll(`.item`);
 const numberElements = document.querySelectorAll(`.number`);
 const operatorElements = document.querySelectorAll(`.operator`);
 const outputElement = document.querySelector(`.output__main`);
-
+const inputElement = document.querySelector(`input`);
+let numsRegEx = RegExp(/\d+/gi);
+let operatorsRegEx = RegExp(/\+|\*|\-|\//gi);
+let numsAndOperators = RegExp(/\d+|\+|\*|\-|\//gi);
 let output = ``;
-let operator = ``;
+let nums;
+let operators;
+let numsOperators;
 
-const makeNumber = (e) => {
-  output += e.target.textContent;
-  console.log(output);
-  showIt(output);
-};
-const chooseOperator = (e) => {
-  operator += e.target.textContent;
-  console.log(operator, num1);
-};
-
-const performCalculation = () => {
-  if (output) {
-    let num1 = +output;
-
-    showIt(operator);
-  } else {
-    outputElement.textContent = `Insert a number`;
-  }
-};
-
+// func
 const showIt = (e) => {
   output += e.target.textContent;
-  outputElement.textContent += output;
+  console.log(output);
+  nums = output.match(numsRegEx);
+  operators = output.match(operatorsRegEx);
+  numsOperators = output.match(numsAndOperators);
+  console.log(nums);
+  console.log(operators);
+  console.log(numsOperators);
+
+  inputElement.value = output;
 };
 
-operatorElements.forEach((item) => {
-  item.addEventListener(`click`, chooseOperator);
-});
-numberElements.forEach((item) => {
-  item.addEventListener(`click`, makeNumber);
-});
+// events
 itemElements.forEach((item) => {
   item.addEventListener(`click`, showIt);
 });
