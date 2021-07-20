311cf7df8ea2a45771e066a126ec3f9bbd66c1e1
st-182 <31894656+st-182@users.noreply.github.com>
2021-07-20 09:13:57 +0300

    AutopliusProject started

---
+++ week 15/19.07.2021/AutopliusTask/backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+//Routes
+app.get(`/`, (req, res) => res.send(`API is running`));
+
+//Starting server
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
